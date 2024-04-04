3020c7744316460d8d4f8d6e51085661812cc47d
Alireza Atashnejad <32798129+Mratashnejad@users.noreply.github.com>
2024-04-05 00:00:02 +0400

    404

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -5,10 +5,18 @@ export default function userId(){
 
     console.log(router.pathname)
     console.log(router.query)
+
+    function showUserIdHandler(){
+       router.push({
+        pathname:'/users/[id]',
+        query: { id:'xashi'},
+       })
+    }
     
     return(
         <div>
             <h2>user Id  </h2>
+            <button onClick={showUserIdHandler}>show User id</button>
         </div>
     )
 }
